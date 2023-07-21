953d06d3b7d171df38170b406f7b1775a4a72cdb
freezedoge <138511488+freezedoge@users.noreply.github.com>
2023-07-21 21:01:52 +0800

    Add files via upload

---
+++ Portfolio3/main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["Frontend Developer","Gamer","Cat Lover","Web Programmer"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:500,
+    loop:true
+});
